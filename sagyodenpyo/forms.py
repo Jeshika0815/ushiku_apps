b20c57ad7ec2d31a1d6d6b1bc9b55a4b48f61634
Jeshika 0815 <91335738+Jeshika0815@users.noreply.github.com>
2025-05-27 15:01:44 +0900

    Sagyodenpyo_uploaded_at-27.5.25

--- sagyodenpyo/forms.py
+++ sagyodenpyo/forms.py
@@ -1,7 +1,55 @@
 from django import forms
 from .models import WorkLog
+from sagyoshiji.models import WorkOrder
 
 class WorkLogForm(forms.ModelForm):
+    work_number = forms.ChoiceField(
+        choices=[('', '----')] + [(work_number, work_number) for work_number in WorkOrder.objects.filter(work_number__isnull=False).values_list('work_number', flat=True).distinct().order_by('-release_date')],
+        label='工番',
+        widget=forms.Select(attrs={'class': 'form-control'})
+    )
+    work_trenum = forms.ChoiceField(
+        choices=[('', '----')] + [(work_trenum, work_trenum) for work_trenum in WorkOrder.objects.filter(work_trenum__isnull=False, work_number__isnull=False).values_list('work_trenum', flat=True).distinct().order_by('-release_date')],
+        label='枝番',
+        widget=forms.Select(attrs={'class': 'form-control'})
+    )
+    subject = forms.ChoiceField(
+        choices=[('', '----')] + [(subject, subject) for subject in WorkOrder.objects.filter(subject__isnull=False).values_list('subject', flat=True).distinct().order_by('-release_date')],
+        label='件名',
+        widget=forms.Select(attrs={'class': 'form-control'})
+    )
+    """
+    work_number = forms.ModelChoiceField(
+        queryset = WorkOrder.objects.filter(work_number__isnull=False).values_list('work_number', flat=True).distinct().order_by('-release_date'),
+        empty_label="----",
+        label='工番',
+        widget=forms.Select(attrs={'class': 'form-control'})
+    )
+    work_trenum = forms.ModelChoiceField(
+        queryset=WorkOrder.objects.filter(work_trenum__isnull=False, work_number__isnull=False).values_list('work_trenum', flat=True).distinct().order_by('-release_date'),
+        empty_label="----",
+        label='枝番',
+        widget=forms.Select(attrs={'class': 'form-control'})
+    )
+    subject = forms.ModelChoiceField(
+        queryset=WorkOrder.objects.filter(subject__isnull=False).values_list('subject', flat=True).distinct().order_by('-release_date'),
+        empty_label="----",
+        label='件名',
+        widget=forms.Select(attrs={'class': 'form-control'})
+    )
+    
+    def __init__(self, *args, **kwargs):
+        work_number = kwargs.pop('work_number', None)
+        super().__init__(*args, **kwargs)
+        if work_number:
+            self.fields['work_trenum'].queryset = WorkOrder.objects.filter(
+                work_number=work_number,
+                work_trenum__isnull = False
+            ).distinct().order_by('-release_date')
+        else:
+            self.fields['work_trenum'].queryset = WorkOrder.objects.none()
+    """
+
     class Meta:
         model = WorkLog
         fields = ['date', 'work_number', 'work_trenum', 'subject', 'work_code', 'work_hours', 'work_minute']
@@ -19,3 +67,6 @@ class WorkLogForm(forms.ModelForm):
             'work_hours': forms.NumberInput(attrs={'min':'0','max':'23'}),
             'work_minute': forms.NumberInput(attrs={'step': '10', 'min':'0', 'max':'50'}),
         }
+
+
+        
